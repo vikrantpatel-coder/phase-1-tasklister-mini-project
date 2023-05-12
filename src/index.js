b03b7934d2be808d2b58741976d7c8ec701b2029
vikrantpatel-coder <126751705+vikrantpatel-coder@users.noreply.github.com>
2023-05-12 19:01:47 -0400

    taskLister_lab_complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = documet.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buildToDo(e.target.new_node.value);
+    form.reset();
+  });
 });
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
+
+function buildToDo(todo) {
+  let p = document.createElement("p");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = "X";
+  p.textContent = `${todo}`;
+  p.appendChild(btn);
+  document.querySelector("#todo_container").appendChild(p);
+}
